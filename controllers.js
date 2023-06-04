c6f47942cfd0e7d527a1bc6bbeadcb4c59d17161
Athaya Reyhan <athreyhan04@gmail.com>
2023-06-04 16:40:32 +0700

    msg

--- controllers.js
+++ controllers.js
@@ -1,12 +1,62 @@
 const grpc = require("@grpc/grpc-js");
 const protoLoader = require("@grpc/proto-loader");
 
-const pool = require("./database/pool.js")
+const pool = require("./pool.js")
 
-const packageDefinition = protoLoader.loadSync("gate.proto");
-const gate = grpc.loadPackageDefinition(packageDefinition).gate;
+const packageDefinition = protoLoader.loadSync("./gate.proto", {
+  keepCase: true,
+  defaults: true,
+  enums: String,
+  oneofs: true,
+});
 
-const server = new grpc.Server();
+const grpcObject = grpc.loadPackageDefinition(packageDefinition)
+
+const api = grpcObject.api
+
+const server = new grpc.Server()
+
+const login = (table, isvalid, idgate, idkartu) => {
+  let query = `INSERT INTO ${table} (id_kartu_akses, id_register_gate, is_valid) VALUES (${idkartu}, ${idgate}, ${isvalid})`;
+  pool.request().query(query, (err, res) => {
+    if (err) throw err;
+
+    console.log(res);
+  });
+};
+
+
+
+server.addService(api.Posts.service, {
+  keluar(call, callback) {
+  const id_kartu_akses = call.request.id_kartu_akses;
+  console.log("(KELUAR) Melakukan Post  dengan ID: " + id_kartu_akses);
+
+  // Your logic for handling the keluar request
+
+  const response = { message: "Your response message" };
+  callback(null, response);
+},
+  masuk(call, callback) {
+  const id_kartu_akses = call.request.id_kartu_akses;
+  const id_register_gate = call.request.id_register_gate;
+  console.log(
+    "(MASUK) Melakukan Post dengan ID: " +
+      id_kartu_akses +
+      " dan " +
+      id_register_gate
+  );
+
+  // Your logic for handling the masuk request
+
+  const response = { message: "Your response message" };
+  callback(null, response);
+}
+})
+
+
+
+
 
 function keluar(call, callback) {
   const id_kartu_akses = call.request.id_kartu_akses;
@@ -33,8 +83,6 @@ function masuk(call, callback) {
   const response = { message: "Your response message" };
   callback(null, response);
 }
-
-server.addService(gate.posts.service, { keluar, masuk });
 
 server.bindAsync(
   "0.0.0.0:50051",
